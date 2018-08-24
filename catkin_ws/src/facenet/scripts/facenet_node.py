218b435d107669a057a459207d1a610c3f5526df
rmartella <reymar_44@hotmail.com>
2018-08-23 19:12:10 -0500

    Add the text and correct face recognition

--- catkin_ws/src/facenet/scripts/facenet_node.py
+++ catkin_ws/src/facenet/scripts/facenet_node.py
@@ -16,50 +16,7 @@ import align.detect_face
 import numpy as np
 import math
 import pickle
-
-def get_model_filenames(model_dir):
-    files = os.listdir(model_dir)
-    meta_files = [s for s in files if s.endswith('.meta')]
-    if len(meta_files)==0:
-        raise ValueError('No meta file found in the model directory (%s)' % model_dir)
-    elif len(meta_files)>1:
-        raise ValueError('There should not be more than one meta file in the model directory (%s)' % model_dir)
-    meta_file = meta_files[0]
-    ckpt = tf.train.get_checkpoint_state(model_dir)
-    if ckpt and ckpt.model_checkpoint_path:
-        ckpt_file = os.path.basename(ckpt.model_checkpoint_path)
-        return meta_file, ckpt_file
-
-    meta_files = [s for s in files if '.ckpt' in s]
-    max_step = -1
-    for f in files:
-        step_str = re.match(r'(^model-[\w\- ]+.ckpt-(\d+))', f)
-        if step_str is not None and len(step_str.groups())>=2:
-            step = int(step_str.groups()[1])
-            if step > max_step:
-                max_step = step
-                ckpt_file = step_str.groups()[0]
-    return meta_file, ckpt_file
-
-def load_model(model, input_map=None):
-    # Check if the model is a model directory (containing a metagraph and a checkpoint file)
-    #  or if it is a protobuf file with a frozen graph
-    model_exp = os.path.expanduser(model)
-    if (os.path.isfile(model_exp)):
-        print('Model filename: %s' % model_exp)
-        with gfile.FastGFile(model_exp,'rb') as f:
-            graph_def = tf.GraphDef()
-            graph_def.ParseFromString(f.read())
-            tf.import_graph_def(graph_def, input_map=input_map, name='')
-    else:
-        print('Model directory: %s' % model_exp)
-        meta_file, ckpt_file = get_model_filenames(model_exp)
-        
-        print('Metagraph file: %s' % meta_file)
-        print('Checkpoint file: %s' % ckpt_file)
-      
-        saver = tf.train.import_meta_graph(os.path.join(model_exp, meta_file), input_map=input_map)
-        saver.restore(tf.get_default_session(), os.path.join(model_exp, ckpt_file))
+import facenet
 
 def facenet_align(cv_image):
     #print("facnet align function")
@@ -94,6 +51,8 @@ def facenet_align(cv_image):
     return det_arr
 
 def facenet_recognition(det_arr, cv_image):
+    best_class_name = []
+    class_probabilities = []
     with facenetGraph.as_default(): 
         with facenetSession.as_default() as sess:
             img_size = np.asarray(cv_image.shape)[0:2]
@@ -123,7 +82,8 @@ def facenet_recognition(det_arr, cv_image):
                     bb[3] = np.minimum(det[3] + margin/2, img_size[0])
                     cropped = cv_image[bb[1]:bb[3], bb[0]:bb[2],:]
                     scaled = misc.imresize(cropped, (image_size, image_size), interp='bilinear')
-                    images.append(scaled)
+                    image = facenet.prewhiten(scaled)
+                    images.append(image)
                 feed_dict = { images_placeholder:images, phase_train_placeholder:False }
                 emb_array[start_index:end_index,:] = sess.run(embeddings, feed_dict=feed_dict)
             
@@ -132,9 +92,13 @@ def facenet_recognition(det_arr, cv_image):
             best_class_indices = np.argmax(predictions, axis=1)
             print (best_class_indices)
             best_class_probabilities = predictions[np.arange(len(best_class_indices)), best_class_indices]
-                
+               
             for i in range(len(best_class_indices)):
                 print('%4d  %s: %.3f' % (i, class_names[best_class_indices[i]], best_class_probabilities[i]))
+                best_class_name.append(class_names[best_class_indices[i]])
+                class_probabilities.append(best_class_probabilities[i])
+
+    return (best_class_name, class_probabilities)
     
 def image_callback(data):
         try:
@@ -145,31 +109,33 @@ def image_callback(data):
         det_arr = facenet_align(cv_image)
         img_size = np.asarray(cv_image.shape)[0:2]
 
-        nrof_successfully_aligned = 0
 
-        for i, det in enumerate(det_arr):
-            det = np.squeeze(det)
-            bb = np.zeros(4, dtype=np.int32)
-            bb[0] = np.maximum(det[0] - margin/2, 0)
-            bb[1] = np.maximum(det[1] - margin/2, 0)
-            bb[2] = np.minimum(det[2] + margin/2, img_size[1])
-            bb[3] = np.minimum(det[3] + margin/2, img_size[0])
-            #cropped = cv_image[bb[1]:bb[3], bb[0]:bb[2],:]
-            #scaled = misc.imresize(cropped, (image_size, image_size), interp='bilinear')
-            cv2.rectangle(cv_image,(bb[0], bb[1]),(bb[2],bb[3]),(0,255,0),3)
-            nrof_successfully_aligned += 1
-            #filename_base, file_extension = os.path.splitext(output_filename)
-            #if args.detect_multiple_faces:
-            #    output_filename_n = "{}_{}{}".format(filename_base, i, file_extension)
-            #else:
-            #output_filename_n = "{}{}".format(filename_base, file_extension)
-            #misc.imsave(output_filename_n, scaled)
-            #text_file.write('%s %d %d %d %d\n' % (output_filename_n, bb[0], bb[1], bb[2], bb[3]))
-
-        if nrof_successfully_aligned > 0:
-            facenet_recognition(det_arr, cv_image)
-        
-        cv2.imshow("Image", cv_image)
+        if len(det_arr) > 0:
+            (best_class_name, class_probabilities) = facenet_recognition(det_arr, cv_image)
+            for i, det in enumerate(det_arr):
+                det = np.squeeze(det)
+                bb = np.zeros(4, dtype=np.int32)
+                bb[0] = np.maximum(det[0] - margin/2, 0)
+                bb[1] = np.maximum(det[1] - margin/2, 0)
+                bb[2] = np.minimum(det[2] + margin/2, img_size[1])
+                bb[3] = np.minimum(det[3] + margin/2, img_size[0])
+                #cropped = cv_image[bb[1]:bb[3], bb[0]:bb[2],:]
+                cv2.rectangle(cv_image,(bb[0], bb[1]),(bb[2],bb[3]),(0,255,0),3)
+                if class_probabilities[i] > 0.92:
+                    cv2.putText(cv_image, best_class_name[i].split(' ')[0], (bb[0], bb[3]), cv2.FONT_HERSHEY_SIMPLEX, 1.0,(0,255,255),1,cv2.LINE_AA)
+                else:
+                    cv2.putText(cv_image, 'Unknown', (bb[0], bb[3]), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,255,255),1,cv2.LINE_AA)
+                #nrof_successfully_aligned += 1
+                #filename_base, file_extension = os.path.splitext(output_filename)
+                #if args.detect_multiple_faces:
+                #    output_filename_n = "{}_{}{}".format(filename_base, i, file_extension)
+                #else:
+                #output_filename_n = "{}{}".format(filename_base, file_extension)
+                #misc.imsave(output_filename_n, scaled)
+                #text_file.write('%s %d %d %d %d\n' % (output_filename_n, bb[0], bb[1], bb[2], bb[3]))
+                
+  
+        cv2.imshow("Image", cv_image[...,::-1])
         cv2.waitKey(3)
 
 def main(args):
@@ -213,7 +179,7 @@ def main(args):
     with facenetGraph.as_default():
         facenetSession = tf.Session()
         with facenetSession.as_default() as sess:
-            load_model(model_file)
+            facenet.load_model(model_file)
             classifier_filename_exp = os.path.expanduser(classifier_file)
             with open(classifier_filename_exp, 'rb') as infile:
                 (model, class_names) = pickle.load(infile)
